ebfcdee170ebf805266e2b5c568e6f1d1507798a
Vishwa Anuj <vishwaanuj@gmail.com>
2023-12-12 17:10:37 +0530

    "second commit "

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,15 @@
 <script>
   import svelteLogo from "./assets/svelte.svg";
   import viteLogo from "/vite.svg";
- 
+
   import Counter from "./lib/Counter.svelte";
   import pattern from "./assets/svelte.svg";
 
+  // Define the URL for Doppler 3D model
+  const dopplerModelUrl = "./doppler.gltf";
+
+  const markerPosition = { x: 0, y: 0, z: 200 };
+  const textSize = { x: 1.5, y: 1.5, z: 1.5 };
 </script>
 
 <a-scene
@@ -12,26 +17,21 @@
   renderer="logarithmicDepthBuffer: true;"
   arjs="debugUIEnabled: false;sourceWidth:1280; sourceHeight:960; displayWidth: 1280; displayHeight: 960;"
 >
-  <!-- a-nft is the anchor that defines an Image Tracking entity -->
-  <!-- on 'url' use the path to the Image Descriptors created before. -->
-  <!-- the path should end with the name without the extension e.g. if file is trex.fset' the path should end with trex -->
-  <a-nft
-    type="nft"
-    url="./LOGO_DESC/logo_b"
-    smooth="true"
-    smoothCount="1"
-    smoothTolerance=".01"
-    smoothThreshold="5"
-  >
-    <!-- as a child of the a-nft entity, you can define the content to show. here's a GLTF model entity -->
-    <a-entity
-      gltf-model="doppler.gltf"
-      scale="5 5 5"
-      position="100 10 0"
-    >
-    </a-entity>
-  </a-nft>
-  <!-- static camera that moves according to the device movemenents -->
+  <a-assets timeout="7000">
+   
+    <img id="osmf-gif" alt="" src="./osmf.gif" />
+  </a-assets>
+  <a-marker type="pattern" url="./0.patt">
+    <!-- White plane as a background for the text -->
+   
+      
+    <a-entity  shadow="cast: true" gltf-model="./doppler.glb" position="0 0 -20" scale="0.5 0.5 0.5" ><a-entity/>
+   
+ 
+
+    <!-- a-text positioned on the white plane -->
+  </a-marker>
+
   <a-entity camera></a-entity>
 </a-scene>
 
@@ -42,12 +42,15 @@
     will-change: filter;
     transition: filter 300ms;
   }
+
   .logo:hover {
     filter: drop-shadow(0 0 2em #646cffaa);
   }
+
   .logo.svelte:hover {
     filter: drop-shadow(0 0 2em #ff3e00aa);
   }
+
   .read-the-docs {
     color: #888;
   }
