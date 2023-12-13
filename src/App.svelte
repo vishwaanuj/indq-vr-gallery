c1f21880c16ce708190d1c07cb1a4483872baeea
Vishwa Anuj <vishwaanuj@gmail.com>
2023-12-13 15:31:49 +0530

    "ar markers pushed "

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,4 @@
 <script>
-  import svelteLogo from "./assets/svelte.svg";
-  import viteLogo from "/vite.svg";
-
-  import Counter from "./lib/Counter.svelte";
-  import pattern from "./assets/svelte.svg";
-
   // Define the URL for Doppler 3D model
   const dopplerModelUrl = "./doppler.gltf";
 
@@ -18,40 +12,33 @@
   arjs="debugUIEnabled: false;sourceWidth:1280; sourceHeight:960; displayWidth: 1280; displayHeight: 960;"
 >
   <a-assets timeout="7000">
-   
-    <img id="osmf-gif" alt="" src="./osmf.gif" />
+    <a-asset-item id="dopplerModel" src="./doppler.gltf"></a-asset-item>
   </a-assets>
+
   <a-marker type="pattern" url="./0.patt">
     <!-- White plane as a background for the text -->
-   
-      
-    <a-entity  shadow="cast: true" gltf-model="./doppler.glb" position="0 0 -20" scale="0.5 0.5 0.5" ><a-entity/>
-   
- 
 
-    <!-- a-text positioned on the white plane -->
+    <a-entity
+      gltf-model="./GLB_MODELS/ORCHEST.glb"
+      rotate="0 -10 0"
+      scale="20 20 20"
+    >
+    </a-entity>
+  </a-marker>
+
+  <a-marker type="pattern" url="./MARKERS/1.patt">
+    <!-- White plane as a background for the text -->
+
+    <a-entity
+      gltf-model="./GLB_MODELS/bed.glb"
+      rotate="0 -10 0"
+      scale="1 1 1"
+    >
+    </a-entity>
   </a-marker>
 
   <a-entity camera></a-entity>
 </a-scene>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-
-  .read-the-docs {
-    color: #888;
-  }
 </style>
